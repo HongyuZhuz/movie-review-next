606c8ecdcdd26c6aa5ff878d5d209f5ff35f3c08
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-04-15 13:46:45 +1000

    added some key in the map of card List

--- src/app/ui/movies/cardList.tsx
+++ src/app/ui/movies/cardList.tsx
@@ -7,7 +7,7 @@ export default function CardList ({movies}:{movies:Movie[]}) {
     return(
     <div className="flex flex-wrap gap-4 mt-4 justify-evenly mx-4">
         {movies.map((movie=>{
-            return <Card movie={movie}/>
+            return <Card key = {movie._id} movie={movie}/>
         }))}
         
     </div>)
