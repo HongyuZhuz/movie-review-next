ce19bf3e8ee0afa67bd7d5eced8e132602fc1a94
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-04-22 09:45:31 +1000

    write the fetch Movie By id method

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,18 +1,28 @@
 import axios from "axios";
-import { Movie } from "./definition";
+import { Movie,ApiFetchMovies } from "./definition";
 import { error } from "console";
-import { ApiFetchMovies } from "./definition";
+
 export default class Data{
       static async fetchMovies(page:number=0):Promise<Movie[]> {
         try{
            const response = await axios.get<ApiFetchMovies>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws/api/v1/movies?page=${page}`)
            const movieList = response.data.movies;
-           console.log(movieList)
            return movieList
-
         }catch{
             console.error("fetch movieList failed:",error);
             throw error
         }
+     }
+
+     static async fetchMovieById(id:string):Promise<Movie>{
+      try{
+         const response = await axios.get<Movie>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws//api/v1/movies/id/${id}`)
+         const movie = response.data;
+         console.log(movie)
+         return movie;
+      }catch{
+         console.error("fetch movie by id failed:",error);
+         throw error
+      }
      }
 }
