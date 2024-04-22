5f47ffabe37bbed04ce2065317ff651cb3dc4e3d
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-04-22 10:34:06 +1000

    design the movie detail page, bug still have bugs

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { usePathname } from "next/navigation";
 import Data from "@/app/lib/data";
+import Image from "next/image";
 
 export default async function Home() {
     const pathname=usePathname();
@@ -8,12 +9,40 @@ export default async function Home() {
     const movie = await Data.fetchMovieById(id);
 
       return (
-        <div>
-          <div>
-            {movie.title}
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <div className="text-lg">
+              {movie.title}
+            </div>
+            <div className="flex flex-row h-1/2">
+              <div className="basis-1/2 relative">
+                {movie.poster?
+                    <Image className=" shadow-lg" src={movie.poster} alt="Picture of the movie"layout="fill" objectFit="cover"></Image>:
+                    <Image className=" shadow-lg" src="/noimage.png" alt="Picture of the movie" width={300} height={450} layout="responsive"></Image>
+                }
+              </div>
+              
+              <div className="flex flex-col basis-1/2">
+                <h1>Description</h1>
+                <div>
+                  {movie.fullplot}
+                </div>
+                <div>
+                  {movie.cast}
+                </div>
+              </div>
+            </div>
+            
+            
           </div>
-          <div>
 
+
+          <br/>
+
+          <div className="reviewSection">
+            <h1>
+              Reviews
+            </h1>
           </div>
         </div>
       );
