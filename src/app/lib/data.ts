c21bd829b5042c048e1d80ab5c9705c723f434bc
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-04-22 10:02:37 +1000

    movies can fetch the details by id

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -8,7 +8,7 @@ export default class Data{
            const response = await axios.get<ApiFetchMovies>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws/api/v1/movies?page=${page}`)
            const movieList = response.data.movies;
            return movieList
-        }catch{
+        }catch(error){
             console.error("fetch movieList failed:",error);
             throw error
         }
@@ -16,11 +16,11 @@ export default class Data{
 
      static async fetchMovieById(id:string):Promise<Movie>{
       try{
-         const response = await axios.get<Movie>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws//api/v1/movies/id/${id}`)
+         const response = await axios.get<Movie>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws/api/v1/movies/id/${id}`)
          const movie = response.data;
          console.log(movie)
          return movie;
-      }catch{
+      }catch(error){
          console.error("fetch movie by id failed:",error);
          throw error
       }
--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 import { usePathname } from "next/navigation";
+import Data from "@/app/lib/data";
 
-export default function Home() {
-    const pathName=usePathname()
+export default async function Home() {
+    const pathname=usePathname();
+    const id = pathname.replace("/movies/","");
+    const movie = await Data.fetchMovieById(id);
+
       return (
         <div>
-          {pathName}
+          <div>
+            {movie.title}
+          </div>
+          <div>
+
+          </div>
         </div>
       );
     }
