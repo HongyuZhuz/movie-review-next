d971dfb75cc3193984b22013b300afe5b1a70432
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-04-24 14:26:37 +1000

    I have setted the search component as changing the domain

--- src/app/ui/form.tsx
+++ src/app/ui/form.tsx
@@ -1,10 +1,22 @@
 'use client'
 import React from 'react'; 
 import {ratings} from '../lib/placeholder-data'
+import {useSearchParams, usePathname, useRouter} from 'next/navigation';
 
 export default function Form() {
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value); 
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const{replace} = useRouter();
+
+    const handleSearch = (term:string,a:string) => {
+        const params = new URLSearchParams(searchParams);
+        if(term){
+            params.set(a, term);
+        }else{
+            params.delete(a)
+        }
+        console.log(term); 
+        replace(`${pathname}?${params.toString()}`)
     };
 
     return (
@@ -12,10 +24,16 @@ export default function Form() {
             <input
                 type="text"
                 placeholder="movie name"
-                onChange={handleNameChange} 
+                onChange={(e)=>{
+                    handleSearch(e.target.value,"title")
+                }}
                 className='rounded-md my-.5'
+                defaultValue={searchParams.get('title')?.toString()}
             />
-            <select defaultValue={"ALL"} className='rounded-md my-1'>
+            <select defaultValue={"ALL"} className='rounded-md my-1' onChange={(e)=>{
+                    handleSearch(e.target.value,"rated")
+                }}
+                value={searchParams.get('rated')?.toString()}>
                 {ratings.map((rating)=>{
                     return <option key={rating} value={rating}>{rating}</option>
                 })}
