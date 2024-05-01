365fd9d05ed95ed5385ef5a6f97f33bf64d9a95a
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-01 13:39:34 +1000

    change looks of cardlist for mobile phone

--- src/app/ui/movies/cardList.tsx
+++ src/app/ui/movies/cardList.tsx
@@ -4,7 +4,7 @@ import { Movie } from "@/app/lib/definition"
 
 export default function CardList ({movies}:{movies:Movie[]}) {
     return(
-    <div className="flex flex-wrap gap-4 mt-4  mx-4">
+    <div className="flex flex-wrap gap-4 mt-4  mx-4 md:justify-normal justify-center">
         {movies.map((movie=>{
             return <Card key = {movie._id} movie={movie}/>
         }))}
