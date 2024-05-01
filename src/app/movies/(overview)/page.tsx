27dd9c5d2e730ca2e448bbbce841a3d868f8385f
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-01 12:30:59 +1000

    fix some bugs but still need the figure out how to use suspense

--- src/app/movies/(overview)/page.tsx
+++ src/app/movies/(overview)/page.tsx
@@ -1,16 +1,20 @@
 import CardList from "../../ui/movies/cardList";
-import Form from "../../ui/form";
 import Data from "../../lib/data";
 import { Suspense } from "react";
+import { unstable_noStore as noStore } from 'next/cache';
+
 
 export default async function Home({ searchParams }: { searchParams?: { title?: string; rated?: string; page?: number } }) {
-  const movieList = await Data.fetchMovies(searchParams?.page, searchParams?.title, searchParams?.rated);
-
   return (
     <div>
     <Suspense fallback={<div>Loading...</div>}>
-        <CardList movies={movieList} />
+        <FetchCardListData searchParams={searchParams}/>
     </Suspense>
     </div>
   );
+}
+async function FetchCardListData({ searchParams }: { searchParams?: { title?: string; rated?: string; page?: number } }){
+  const movieList = await Data.fetchMovies(searchParams?.page, searchParams?.title, searchParams?.rated);
+
+  return (<CardList movies={movieList}/>);
 }
--- src/app/ui/movies/card.tsx
+++ src/app/ui/movies/card.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 
 
 export default async function Card ({movie}:{movie:Movie}) {
-    let movieData = await fetchMovieData(movie);
 
     return(
         <Link className="flex-auto flex flex-col  justify-start border w-72 p-5 rounded-md border-sky-600 hover:bg-sky-200 shadow-xl" href={`/movies/${movie._id}`}>
@@ -28,20 +27,3 @@ export default async function Card ({movie}:{movie:Movie}) {
     </Link>
 )
 }
-
-async function fetchMovieData(movieData:Movie) {
-    if (movieData.poster){
-        movieData.poster = await checkImageURL(movieData.poster) ? movieData.poster : '/noimage.png';
-    }
-    return movieData;
-  }
-  
-  
-async function checkImageURL(url: string): Promise<boolean> {
-    try {
-      const response = await fetch(url, { method: 'HEAD' });
-      return response.ok;
-    } catch {
-      return false;
-    }
-  }
