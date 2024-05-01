7c725a60422a6a5c3d3b5b58c605f52af21ba442
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-01 11:14:09 +1000

    usint loading.tsx to make domain jump smoothly

--- src/app/movies/(overview)/page.tsx
+++ src/app/movies/(overview)/page.tsx
@@ -1,6 +1,6 @@
-import CardList from "../ui/movies/cardList";
-import Form from "../ui/form";
-import Data from "../lib/data";
+import CardList from "../../ui/movies/cardList";
+import Form from "../../ui/form";
+import Data from "../../lib/data";
 import { Suspense } from "react";
 
 export default async function Home({ searchParams }: { searchParams?: { title?: string; rated?: string; page?: number } }) {
