77347a6f42610f74d8cbdbbab922c1e4bd38eb72
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-02 14:05:51 +1000

    delete data settings

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -5,7 +5,6 @@ import { unstable_noStore as noStore } from 'next/cache';
 
 export async function  fetchMovies(page:number=0 ,title:string="", rated:string=""):Promise<Movie[]> {
          noStore();
-         await new Promise((resolve) => setTimeout(resolve, 3000));
         try{
          console.log(`Fetching movies with page=${page}, title=${title}, rated=${rated}`);
            const response = await axios.get<ApiFetchMovies>(`https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws/api/v1/movies?page=${page}&title=${title}&rated=${rated}`)
