0250d81d9bde23ce2b5fb2b1b0427fed3813be70
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-03 12:49:44 +1000

    git some styles for the page button

--- src/app/ui/movies/pagination.tsx
+++ src/app/ui/movies/pagination.tsx
@@ -4,11 +4,13 @@
 export  default function Pagination({totalNumMovies,moviesPerPage}:{totalNumMovies:number,moviesPerPage:number}) {
     const pageNum = Math.ceil(totalNumMovies/ moviesPerPage);
     const buttons = Array.from({ length: pageNum }, (_, index) => index + 1).map(page => (
-        <button key={page} onClick={() => console.log(`Go to page ${page}`)}>
+        <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        key={page} onClick={() => console.log(`Go to page ${page}`)}>
           Page {page}
         </button>
       ));
     return(
-        <div>{pageNum}</div>
+        <div className="flex space-x-2 mt-3">{buttons}</div>
     )
 }
