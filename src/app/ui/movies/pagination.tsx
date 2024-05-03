90bdd7b3a33ffe33f3ab508257351100e66fd5a8
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-03 13:11:28 +1000

    now we can use page button for pagination

--- src/app/ui/movies/pagination.tsx
+++ src/app/ui/movies/pagination.tsx
@@ -1,15 +1,22 @@
-
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 
 export  default function Pagination({totalNumMovies,moviesPerPage}:{totalNumMovies:number,moviesPerPage:number}) {
     const pageNum = Math.ceil(totalNumMovies/ moviesPerPage);
-    const buttons = Array.from({ length: pageNum }, (_, index) => index + 1).map(page => (
-        <button 
+    const currentPath = usePathname();
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+
+    const buttons = Array.from({ length: pageNum }, (_, index) => index + 1).map(page => {
+        params.set('page',(page-1).toString())
+        return(<Link
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        key={page} onClick={() => console.log(`Go to page ${page}`)}>
+        key={page} onClick={() => console.log(`Go to page ${page}`)}
+        href={`${currentPath}?${params.toString()}`}>
           Page {page}
-        </button>
-      ));
+        </Link>
+      )})
     return(
         <div className="flex space-x-2 mt-3">{buttons}</div>
     )
