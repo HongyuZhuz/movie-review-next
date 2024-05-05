3240366109ce71006807da8c95e377ee6db978c8
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-05 22:25:06 +1000

    fix a little bit bugs

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,20 +1,15 @@
 
 import FetchCardListData from "@/app/ui/movies/fetchCardList";
-import { fetchMovies } from "@/app/lib/data";
-import { Suspense } from "react";
-
 
 export default function Home({ searchParams }: { searchParams?: { title?: string; rated?: string; page?: number } }) {
   const page = searchParams?searchParams.page:undefined;
     const title = searchParams?searchParams.title:undefined;
     const rated = searchParams?searchParams.rated:undefined;
     console.log("here "+" page: "+page +" title: " +title+" rated: "+rated)
-    
 
   return (
     <div>
         <FetchCardListData page={page} title={title} rated={rated} />
-        
     </div>
   );
 }
--- src/app/ui/movies/cardList.tsx
+++ src/app/ui/movies/cardList.tsx
@@ -7,7 +7,7 @@ import { Movie } from "@/app/lib/definition"
 export   default function CardList ({movies}:{movies:Movie[]}) {
     console.log("rendering")
     return(
-    <div className="flex flex-wrap gap-4 mt-4  mx-4 md:justify-normal justify-center">
+    <div className="flex flex-wrap gap-4 m-4  mx-4 md:justify-normal justify-center">
         {movies.map((movie=>{
             return <Card key = {movie._id} movie={movie}/>
         }))}
