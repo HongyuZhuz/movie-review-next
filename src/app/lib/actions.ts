7273efd3a2e3ede686e449625d3e7f4a0b357b92
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-05-25 23:42:44 +1000

    add date and userInfo now

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -9,9 +9,24 @@ const FormSchema = z.object(
         date:z.string(),
     }
 )
-const CreateInvoice = FormSchema.omit({id:true,date:true})
+const CreateInvoice = FormSchema.omit({userInfo:true,date:true})
 
 export async function createReview (id:string, formData:FormData){
-    console.log(id);
-    console.log(formData.get('review'))
+
+    const {movieId, review} = CreateInvoice.parse(
+        {
+            movieId:id,
+            review:formData.get('review'),
+        }
+    )
+    
+
+    const userInfo = {name:"z", _id:"123"};
+    const date = new Date().toISOString().split('T')[0];
+
+    console.log(movieId);
+    console.log(review)
+    console.log(date)
+    console.log(userInfo)
+
 }
