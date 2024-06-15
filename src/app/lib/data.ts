cd31a24ba141f112f7836d32881afa0cc59e7767
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-16 00:49:30 +1000

    change the backend api, and now we can delete reviews

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -53,6 +53,7 @@ export async function  fetchMovies(page:number=0 ,title:string="", rated:string=
       
       try {
         const response = await axios.put("https://jb7iw7mjxgoabj2pm6v6qquoea0zkwli.lambda-url.us-east-1.on.aws/api/v1/movies/review", data);
+        console.log(id)
       } catch (error) {
         if (axios.isAxiosError(error)) {
           console.error('AxiosError:', error.message);
