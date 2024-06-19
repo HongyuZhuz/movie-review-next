b167825af4cb6d999e092ca26dd7e8b61e625ed0
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-19 14:48:55 +1000

    use a loading skeleton for movie detail data fetch

--- src/app/movies/[id]/loading.tsx
+++ src/app/movies/[id]/loading.tsx
@@ -1,10 +1,10 @@
 
-
+import { MovieDetailSkeleton } from "@/app/ui/skeleton/movie-card-skeleton"
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div>
-          Loading........
+          <MovieDetailSkeleton/>
         </div>
     )
   }
--- src/app/ui/skeleton/movie-card-skeleton.tsx
+++ src/app/ui/skeleton/movie-card-skeleton.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Skeleton } from "@douyinfe/semi-ui";
 export const cardSkeleton = (<div className="flex-auto flex flex-col  justify-center border w-72 p-5 rounded-md border-sky-600 max-w-80">
     <Skeleton.Image style={{ width: 250, height: 450 }}/>
@@ -49,3 +50,42 @@ export const movieDetailSkeleton = (<div className="flex flex-col">
   </h1>
 </div>
 </div>)
+
+export function MovieDetailSkeleton () {
+  return(
+    <div className="flex flex-col">
+<div className="flex flex-col h-screen">
+  <div className="text-lg">
+    <Skeleton.Title></Skeleton.Title>
+  </div>
+  <div className="flex flex-col md:flex-row justify-center flex-auto">
+    <div className=" flex justify-center md:max-h-96 border-4">
+        <Skeleton.Image style={{ width: 300, height: 450 }}></Skeleton.Image>
+    </div>
+        
+    <div className="md:w-1/2 p-4 flex flex-col flex-1">
+      <div className=" max-w-80">
+      <Skeleton.Paragraph rows={6}></Skeleton.Paragraph>
+      <div>
+      <Skeleton.Paragraph rows={3}></Skeleton.Paragraph>
+      </div>
+      <div>
+      <Skeleton.Paragraph rows={1}></Skeleton.Paragraph>
+      </div>
+      </div>
+      
+    </div>
+  </div>
+  
+  
+</div>
+<br/>
+
+<div className="reviewSection">
+  <h1>
+    Reviews
+  </h1>
+</div>
+</div>
+  )
+}
