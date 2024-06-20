ab2824777b7e6ca98c5f9705856e55e0786b07ad
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-20 14:01:35 +1000

    have a title now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import { Title } from "./ui/homePage/title";
 export default function Home() {
   return (
-    <div>
-      <p> Hello!</p>
+    <div className="flex justify-center mt-36">
+      <Title/>
     </div>
   );
 }
---
+++ src/app/ui/homePage/title.tsx
@@ -0,0 +1,14 @@
+
+
+export function Title () {
+    return(
+        <div className="flex flex-col max-w-4xl text-center">
+            <div className="text-6xl text-black font-bold m-4">
+                    This is  Hongyu&apos;s practice website
+            </div>
+            <div className="text-2xl text-gray-400 font-normal m-4">
+            A Full-stack website built with Next.js, Express.js, MongoDB and AWS. Designed with Figma.
+            </div>
+        </div>
+    )
+}
