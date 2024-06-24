fd278d249ecf95ddabb825395e0a851c7b3dceee
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-24 12:11:53 +1000

    add an button

--- src/app/ui/homePage/title.tsx
+++ src/app/ui/homePage/title.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link"
 
 export function Title () {
     return(
@@ -9,6 +9,10 @@ export function Title () {
             <div className="text-2xl text-gray-400 font-normal m-4">
             A Full-stack website built with Next.js, Express.js, MongoDB and AWS. Designed with Figma.
             </div>
+            <Link key={"movieList"} href={"/movies"}
+                className="bg-black  text-white py-2 px-3 flex justify-center max-w-28 self-center rounded-md hover:bg-white hover:text-black border-black border-2">
+            Browse</Link>
+            
         </div>
     )
 }
