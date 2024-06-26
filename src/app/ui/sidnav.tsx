f84582bd2bb30d8108d09ec2c2be9a3aa264a2ac
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-26 14:47:19 +1000

    ready for publish

--- src/app/ui/sidnav.tsx
+++ src/app/ui/sidnav.tsx
@@ -6,7 +6,7 @@ export default function SideNav({ratings}:SideNavProps) {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex  items-center space-x-3 rtl:space-x-reverse">
-                    <span className="self-center  text-base font-medium whitespace-nowrap dark:text-white">Hongyu's movie review website</span>
+                    <span className="self-center  text-base font-medium whitespace-nowrap dark:text-white">Hongyu&apos;s movie review website</span>
                 </Link>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
