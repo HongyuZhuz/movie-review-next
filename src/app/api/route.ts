a745c7a4bf19e250008200944aa307573f6c6d81
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-06-30 19:00:58 +1000

    ??

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,4 @@
-
+'use server'
 import { NextResponse, type NextRequest } from 'next/server';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
