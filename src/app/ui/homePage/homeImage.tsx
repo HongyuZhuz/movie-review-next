310ccd1566de1f7e39788f6442b3378cf79af1d7
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-07-01 12:51:53 +1000

    delete the prefix of environment varaiable and add error catch

--- src/app/ui/homePage/homeImage.tsx
+++ src/app/ui/homePage/homeImage.tsx
@@ -2,20 +2,31 @@ import Image from "next/image"
 
 
 export async function HomeImage () {
-    const signedUrl = await getSignedUrl();
     
-    
-    return(
-        <div className="flex m-10">
+        const signedUrl = await getSignedUrl();
+        
+        if(signedUrl===""){
+            return(<div>missing image</div>)
+        }else{
+            return(
+                <div className="flex m-10">
             <Image key={"home image"} src={signedUrl} alt ={signedUrl} width={1280} height={640}/>
-        </div>
-    )
+                </div>
+            )
+        }
+    
 }
 
 async function getSignedUrl() {
-    const response = await fetch(`${process.env.ROOT_URL}/api`,{
-        headers: { 'Cache-Control': 'no-store' },
-      });
-    const data = await response.json();
-    return data.signedUrl;
+    try{
+        const response = await fetch(`${process.env.ROOT_URL}/api`,{
+            headers: { 'Cache-Control': 'no-store' },
+          });
+        const data = await response.json();
+        return data.signedUrl;
+    }catch(e)
+    {
+        return ("");
+    }
+    
   }
