d4d82791a4dc19c81efcbef1e8172b70cdd6d1f7
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-07-05 12:17:38 +1000

    prepare to deploy

---
+++ src/app/movies/[id]/review/[revieId]/page.tsx
@@ -0,0 +1,9 @@
+export default function Home({params}:{params:{id:string}}) {
+    const id = params.id;
+      console.log("start form")
+        return (
+          <div className="flex justify-center rounded-md shadow-lg p-10 items-center m-3">
+            review id
+          </div>
+        );
+      }
