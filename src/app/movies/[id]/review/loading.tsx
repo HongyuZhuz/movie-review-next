8fd4757f934cece6abd79e9e533cf9e97bc332f1
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-07-09 11:56:09 +1000

    finish the loading skeleton

--- src/app/movies/[id]/review/loading.tsx
+++ src/app/movies/[id]/review/loading.tsx
@@ -1,8 +1,9 @@
+import { Load } from "@/app/ui/skeleton/load"
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div>
-          Loading...
+          <Load/>
         </div>
     )
   }
